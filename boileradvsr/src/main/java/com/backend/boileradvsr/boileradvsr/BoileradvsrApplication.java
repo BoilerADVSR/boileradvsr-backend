2a62c4ad9a1c5cc3f277d9eb7dc144e4dfaa03c3
Freddie Clarke <47573673+freddieclarke13@users.noreply.github.com>
2023-02-09 17:53:01 -0500

    Building Spring project

---
+++ boileradvsr/src/main/java/com/backend/boileradvsr/boileradvsr/BoileradvsrApplication.java
@@ -0,0 +1,13 @@
+package com.backend.boileradvsr.boileradvsr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BoileradvsrApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BoileradvsrApplication.class, args);
+	}
+
+}
---
+++ boileradvsr/src/test/java/com/backend/boileradvsr/boileradvsr/BoileradvsrApplicationTests.java
@@ -0,0 +1,13 @@
+package com.backend.boileradvsr.boileradvsr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BoileradvsrApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
