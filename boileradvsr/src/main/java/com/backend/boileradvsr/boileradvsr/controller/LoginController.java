26b4faa3a7436ef826618ef7472ab14dc3504201
Freddie Clarke <47573673+freddieclarke13@users.noreply.github.com>
2023-02-09 20:13:15 -0500

    trying to fix gitignore, commenting out import

--- boileradvsr/src/main/java/com/backend/boileradvsr/boileradvsr/controller/LoginController.java
+++ boileradvsr/src/main/java/com/backend/boileradvsr/boileradvsr/controller/LoginController.java
@@ -2,7 +2,7 @@ package com.backend.boileradvsr.boileradvsr.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
+// import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
