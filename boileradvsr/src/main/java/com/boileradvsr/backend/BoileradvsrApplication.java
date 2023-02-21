99453a05f8d2dd13827f364ec1be05db12f5c753
Freddie Clarke <47573673+freddieclarke13@users.noreply.github.com>
2023-02-21 15:38:16 -0500

    Reset the Spring project to use Gradle

---
+++ boileradvsr/src/main/java/com/boileradvsr/backend/BoileradvsrApplication.java
@@ -0,0 +1,13 @@
+package com.boileradvsr.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BoileradvsrApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BoileradvsrApplication.class, args);
+    }
+
+}
---
+++ boileradvsr/src/test/java/com/boileradvsr/backend/BoileradvsrApplicationTests.java
@@ -0,0 +1,13 @@
+package com.boileradvsr.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BoileradvsrApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
