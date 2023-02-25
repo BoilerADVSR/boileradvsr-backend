62b7102fed74608c846fab3cca885157b112e103
garrettcobrien <gcobrien@purdue.edu>
2023-02-25 16:45:37 -0500

    controller/repository

--- boileradvsr/src/main/java/com/boileradvsr/backend/models/StudentRepository.java
+++ boileradvsr/src/main/java/com/boileradvsr/backend/models/StudentRepository.java
@@ -3,6 +3,5 @@ package com.boileradvsr.backend.models;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
 public interface StudentRepository extends MongoRepository<Student, String> {
-    Student findByFirstName(String firstName);
-
+    public Student findByFirstName(String firstName);
 }
