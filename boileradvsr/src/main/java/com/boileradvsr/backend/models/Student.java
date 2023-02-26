8716664df76b120b7a5ac3a7345d76b11ae36e8d
abdulsaleh10 <abdulsalehali10@gmail.com>
2023-02-25 20:46:59 -0500

    register and login controller

--- boileradvsr/src/main/java/com/boileradvsr/backend/models/Student.java
+++ boileradvsr/src/main/java/com/boileradvsr/backend/models/Student.java
@@ -45,5 +45,75 @@ public class Student {
         degrees.add(degree);
     }
 
-    
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public Semester getGraduationSemester() {
+        return graduationSemester;
+    }
+
+    public void setGraduationSemester(Semester graduationSemester) {
+        this.graduationSemester = graduationSemester;
+    }
+
+    public double getGPA() {
+        return GPA;
+    }
+
+    public void setSemesters(ArrayList<Semester> semesters) {
+        this.semesters = semesters;
+    }
+
+    public ArrayList<Degree> getDegrees() {
+        return degrees;
+    }
+
+    public void setDegrees(ArrayList<Degree> degrees) {
+        this.degrees = degrees;
+    }
+
+    public PlanOfStudy getPlanOfStudy() {
+        return planOfStudy;
+    }
+
+    public void setPlanOfStudy(PlanOfStudy planOfStudy) {
+        this.planOfStudy = planOfStudy;
+    }
+
+    public ArrayList<Advisor> getAcademicAdvisors() {
+        return academicAdvisors;
+    }
+
+    public void setAcademicAdvisors(ArrayList<Advisor> academicAdvisors) {
+        this.academicAdvisors = academicAdvisors;
+    }
 }
--- boileradvsr/src/main/java/com/boileradvsr/backend/models/StudentRepository.java
+++ boileradvsr/src/main/java/com/boileradvsr/backend/models/StudentRepository.java
@@ -1,7 +1,11 @@
 package com.boileradvsr.backend.models;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 
 public interface StudentRepository extends MongoRepository<Student, String> {
-    public Student findByFirstName(String firstName);
+    @Query("{ 'email': '?0' }")
+    public Student findByEmail(String email);
+    @Query("{ 'password': '?0' }")
+    public Student findByPassword(String password);
 }
