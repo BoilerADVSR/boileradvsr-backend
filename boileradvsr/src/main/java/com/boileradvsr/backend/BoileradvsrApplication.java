9aca47eb251d244d4fea84c8228a42ae7b7bc522
garrettcobrien <gcobrien@purdue.edu>
2023-02-26 19:44:48 -0500

    Controller classes for query

--- boileradvsr/src/main/java/com/boileradvsr/backend/BoileradvsrApplication.java
+++ boileradvsr/src/main/java/com/boileradvsr/backend/BoileradvsrApplication.java
@@ -1,5 +1,7 @@
 package com.boileradvsr.backend;
 
+import com.boileradvsr.backend.models.Course;
+import com.boileradvsr.backend.models.CourseRepository;
 import com.boileradvsr.backend.models.Student;
 import com.boileradvsr.backend.models.StudentRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -14,7 +16,10 @@ import org.springframework.data.mongodb.repository.config.EnableMongoRepositorie
 public class BoileradvsrApplication implements CommandLineRunner {
 
     @Autowired
-    private StudentRepository repository;
+    private StudentRepository studentRepository;
+    @Autowired
+    private CourseRepository courseRepository;
+
 
     public static void main(String[] args) {
         SpringApplication.run(BoileradvsrApplication.class, args);
@@ -22,10 +27,22 @@ public class BoileradvsrApplication implements CommandLineRunner {
 
     @Override
     public void run(String... args) throws Exception {
-        repository.deleteAll();
-        repository.save(new Student("Garrett", "O'Brien", "g@purdue.edu", "pw"));
+        studentRepository.deleteAll();
+        courseRepository.deleteAll();
+        studentRepository.save(new Student("Garrett", "O'Brien", "g@purdue.edu", "pw"));
+        studentRepository.save(new Student("Brandon", "hart", "b@purdue.edu", "pw"));
 
-        repository.findByFirstName("Garrett");
+        courseRepository.save(new Course("CS", "307", "Software Engineering", "Computer Science", "Science"));
+        courseRepository.save(new Course("CS", "182", "Discrete Mathematics", "Computer Science", "Science"));
+        courseRepository.save(new Course("MA", "341", "Foundations of Analysis", "Math", "Science"));
+
+        for (Course course : courseRepository.findByCourseIdDepartment("CS")) {
+            System.out.println(course.getCourseID());
+        }
+
+        System.out.println(courseRepository.findCourseByCourseID("MA341").getDepartment());
+
+
     }
 
 }
