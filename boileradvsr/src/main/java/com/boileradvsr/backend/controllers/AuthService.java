d71c7d1dfe17c2a62ce4a0a0cfe40783ea756c34
abdulsaleh10 <abdulsalehali10@gmail.com>
2023-03-12 22:59:22 -0400

    Service for authentication

---
+++ boileradvsr/src/main/java/com/boileradvsr/backend/controllers/AuthService.java
@@ -0,0 +1,47 @@
+package com.boileradvsr.backend.controllers;
+
+import com.boileradvsr.backend.configurations.JwtService;
+import com.boileradvsr.backend.models.Student;
+import com.boileradvsr.backend.models.StudentRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class AuthService {
+
+    private final StudentRepository studentRepository;
+    private final JwtService jwtService;
+    private final PasswordEncoder passwordEncoder;
+    private final AuthenticationManager authenticationManager;
+    public AuthenticationResponse register(RegisterRequest request) {
+        var student = Student.builder()
+                .firstName(request.getFirstName())
+                .lastName(request.getLastName())
+                .email(request.getEmail())
+                .password(passwordEncoder.encode(request.getPassword()))
+                .build();
+        studentRepository.save(student);
+        var jwtToken = jwtService.generateToken(student);
+        return AuthenticationResponse.builder()
+                .token(jwtToken)
+                .build();
+    }
+
+    public AuthenticationResponse login(AuthenticationRequest request) {
+        authenticationManager.authenticate(
+                new UsernamePasswordAuthenticationToken(
+                        request.getEmail(),
+                        request.getPassword()
+                )
+        );
+        var student = studentRepository.findById(request.getEmail()).orElseThrow();
+        var jwtToken = jwtService.generateToken(student);
+        return AuthenticationResponse.builder()
+                .token(jwtToken)
+                .build();
+    }
+}
