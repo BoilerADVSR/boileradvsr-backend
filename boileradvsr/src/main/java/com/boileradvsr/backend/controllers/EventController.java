3957dd00c5e3ff66b35152002f5a3e351ccf25c5
abdul <aasaleh@purdue.edu>
2023-04-21 00:54:55 -0400

    event update

--- boileradvsr/src/main/java/com/boileradvsr/backend/controllers/EventController.java
+++ boileradvsr/src/main/java/com/boileradvsr/backend/controllers/EventController.java
@@ -3,11 +3,15 @@ package com.boileradvsr.backend.controllers;
 import com.boileradvsr.backend.models.DegreeGraph;
 import com.boileradvsr.backend.models.*;
 import com.boileradvsr.backend.models.repositories.EventRepository;
+import com.boileradvsr.backend.models.repositories.StudentRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 import org.springframework.http.ResponseEntity;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.web.bind.annotation.*;
 
 import java.net.URI;
+import java.time.LocalDate;
 import java.util.List;
 
 @CrossOrigin(origins = "http://localhost:3000")
@@ -17,7 +21,50 @@ import java.util.List;
 public class EventController {
     public EventRepository repository;
 
+    @Autowired
+    public StudentRepository srepo;
+
     public EventController(EventRepository repository) {this.repository = repository;}
+
+    @Autowired
+    public EventReminderService reminderService;
+
+    @Scheduled(fixedRate = 86400000, initialDelay = 5000)
+    public void execute() {
+
+        LocalDate localDate = LocalDate.now();
+        int day = localDate.getDayOfMonth();
+        int month = localDate.getMonthValue();
+        int year = localDate.getYear();
+        for (Event r:
+                repository.findAll()) {
+            if (r.getYear() == year) {
+                if (r.getMonth() == month){
+                    if (r.getDay() == day) {
+                        //todo
+                        List<Student> list = srepo.findAll();
+                        reminderService.upcoming(list, r);
+                    }
+                }
+            }
+
+        }
+        localDate = localDate.plusDays(1);
+        day = localDate.getDayOfMonth();
+        month = localDate.getMonthValue();
+        year = localDate.getYear();
+        for (Event r: repository.findAll()) {
+            if (r.getYear() == year) {
+                if (r.getMonth() == month){
+                    if (r.getDay() == day) {
+                        List<Student> list = srepo.findAll();
+                        reminderService.upcoming(list, r);
+                    }
+                }
+            }
+
+        }
+    }
 
     @GetMapping("/all")
     public List<Event> getAllEvents() {
